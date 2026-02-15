bcf3e91a2c9c71c246003b28f85d70ffb7b83c78
Shrestha Chakraborty <137057227+shresh15@users.noreply.github.com>
2026-02-16 00:18:58 +0530

    Time: 1 ms (99.87%), Space: 46.8 MB (82.7%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        // XOR OPERATION
+        //
+        // a ^ a=0
+        //a^0 = a (depends upon a)
+        int ans=0;
+        for (int i:nums){
+            ans=ans ^ i;
+
+        }
+        return ans;
+        
+    }
+}
