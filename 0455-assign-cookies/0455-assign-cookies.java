2c451090cfb08eceb71b15c56bd256565fe72dec
Shrestha Chakraborty <137057227+shresh15@users.noreply.github.com>
2026-06-16 23:03:16 +0530

    Time: 14 ms (93.66%), Space: 51.4 MB (73.42%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0; // child pointer
+        int j = 0; // cookie pointer
+
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                i++; // child is satisfied
+            }
+            j++; // move to next cookie
+        }
+
+        return i; // number of satisfied children
+    }
+}
